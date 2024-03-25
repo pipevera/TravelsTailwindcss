5ccfe0b2db4ed8a30308aa69a82578f18e37d935
Felipe Vera <felipevera10@gmail.com>
2024-03-25 02:27:07 -0300

    finish

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+const buttonDarkMode = document.getElementById("buttonDarkMode");
+
+buttonDarkMode.addEventListener("click", () => {
+    document.documentElement.classList.toggle('dark')
+});
+
+
+
+
+
+/* const toggleDarkMode = () => {
+    document.documentElement.classList.toggle('dark')
+} */
